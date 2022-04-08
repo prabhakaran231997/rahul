a5859a2e38b2015f7b8cd4f0993d031151c94421
prabhakaran231997 <prabhakarancivil23@gmail.com>
2022-04-08 12:09:09 +0530

    done

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,20 @@
+package org.bank;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("savings : 4%");
+	}
+
+	public void fixed() {
+		System.out.println("fixed : 5%");
+	}
+
+	public void deposit() {
+		System.out.println("deposit : 6%");
+	}
+	public static void main(String[] args) {
+		System.out.println("master");
+	}
+
+}
